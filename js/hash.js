9fd15f09add86563a4b9113d84d2d1ca1675ad9a
NASA_Mark <76894588+Atharane@users.noreply.github.com>
2022-07-23 11:48:08 +0530

    add deletion animation

--- js/hash.js
+++ js/hash.js
@@ -4,19 +4,7 @@ const messageEl = document.getElementById("alert-message");
 const crossIcon = document.getElementById("cross-icon");
 const size = 7; // size of hash table, preferably prime
 
-let hashTable = [
-  [],
-  [],
-  [],
-  [],
-  [],
-  [
-    { key: "ath", value: 101 },
-    { key: "hat", value: 102 },
-    { key: "tah", value: 103 },
-  ],
-  [],
-];
+let hashTable = [];
 
 function renderRows(size) {
   let HashTableEl = document.getElementById("hash-table");
@@ -45,13 +33,13 @@ function renderRows(size) {
 
     HashTableEl.appendChild(row_wrapper);
 
-    // hashTable.push([]);
+    hashTable.push([]);
   }
 }
 
 function renderCells(row_index) {
   let rowEl = document.getElementById(`row-${row_index}`);
-  rowEl.innerHTML = ""; 
+  rowEl.innerHTML = "";
 
   let row = hashTable[row_index];
 
@@ -87,7 +75,7 @@ function hash(key) {
 
   for (let i = 0; i < key.length; i++) {
     // ASCII * 10 + index
-    sum += key.charCodeAt(i) * 10 + index;
+    sum += key.charCodeAt(i) * index;
     index++;
   }
   return sum % size;
@@ -107,13 +95,18 @@ function showErrorMessage(message) {
   }, 1500);
 }
 
+// addRedBorder RETURNS A FUNCTION that adds red border to the element
 function addRedBorder(el) {
-  console.log(`adding red borders to cell[${el}]`);
-  el.style.borderColor = "crimson";
+  return () => {
+    el.style.borderColor = "crimson";
+  };
 }
 
+// removeBorder RETURNS A FUNCTION that removes border from the element
 function removeBorder(el) {
-  el.style.borderColor = "#f5f5f7";
+  return () => {
+    el.style.borderColor = "#f5f5f7";
+  };
 }
 
 insertButton.addEventListener("click", () => {
@@ -129,10 +122,10 @@ insertButton.addEventListener("click", () => {
   hashTable[index].unshift({ key: key_el.value, value: value_el.value });
 
   let index_el = document.getElementById(`index-${index}`);
-  addRedBorder(index_el);
+  addRedBorder(index_el)();
 
   setTimeout(() => {
-    removeBorder(index_el);
+    removeBorder(index_el)();
     renderCells(index);
     key_el.value = "";
     value_el.value = "";
@@ -148,44 +141,41 @@ deleteButton.addEventListener("click", () => {
   }
 
   let index = hash(key_el.value);
-  let row = hashTable[index];
-  let len = row.length;
 
-  let rowEl = document.getElementsByClassName("row")[index];
+  let row = hashTable[index];
+  let rowEl = document.getElementById(`row-${index}`);
   let cells = rowEl.children;
 
-  console.log(cells);
-
-  let i = 0;
-  for (i; i < len; i++) {
-    setTimeout(() => {
-      addRedBorder(i);
-    }, 0);
-
-    console.log(i);
-
-    if (row[i].key === key_el.value) {
-      row.splice(i, 1);
-      break;
-    }
-
-    setTimeout(() => {
-      removeBorder(cells[i]);
-    }, (i+1) * 500);
-  }
-
-  // if key doesn't exist, show error message
-  if (i == len || len == 0) {
-    let message_content = document.getElementById("alert-message-content");
-    message_content.textContent = `Key ${key_el.value} not found`;
-    messageEl.style.display = "flex";
-    key_el.value = "";
+  // no entries in the bucket
+  if (cells.length === 0) {
+    showErrorMessage("Key not found");
     return;
   }
 
-  renderCells(index);
-  key_el.value = "";
+  let animationInterval = 400; // milliseconds
+
+  for (var i = 0; i < cells.length; i++) {
+    var child = cells[i];
+
+    setTimeout(addRedBorder(child), i * animationInterval);
+
+    if (row[i].key === key_el.value) {
+      row.splice(i, 1);
+
+      setTimeout(() => {
+        renderCells(index);
+        key_el.value = "";
+      }, (i + 1) * animationInterval);
+
+      break;
+    }
+
+    setTimeout(removeBorder(child), (i + 1) * animationInterval);
+
+    if (i === cells.length - 1) {
+      setTimeout(()=>{showErrorMessage("Key not found")}, (i + 1) * animationInterval);
+    }
+  }
 });
 
 renderRows(size);
-renderCells(5);
